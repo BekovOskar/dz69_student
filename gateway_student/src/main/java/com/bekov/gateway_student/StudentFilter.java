cada931d555ce43342761a2340c1fcf4b59d52f4
BekovOskar <bek.offsll@gmail.com>
2019-06-06 18:43:20 +0600

    create gateway_student

---
+++ gateway_student/src/main/java/com/bekov/gateway_student/StudentFilter.java
@@ -0,0 +1,34 @@
+package com.bekov.gateway_student;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class StudentFilter extends ZuulFilter {
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 1;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        HttpServletRequest request = RequestContext.getCurrentContext().getRequest();
+        logger.info("request -> {}, request uri -> {}", request, request.getRequestURI());
+        return null;
+    }
+}
