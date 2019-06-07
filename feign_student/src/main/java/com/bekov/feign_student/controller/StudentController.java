b826c37540684d20d75a51079d5ef13da27eb9af
BekovOskar <bek.offsll@gmail.com>
2019-06-07 15:50:43 +0600

    added logger

--- feign_student/src/main/java/com/bekov/feign_student/controller/StudentController.java
+++ feign_student/src/main/java/com/bekov/feign_student/controller/StudentController.java
@@ -2,6 +2,8 @@ package com.bekov.feign_student.controller;
 
 import com.bekov.feign_student.model.Student;
 import com.bekov.feign_student.proxy.StudentProxy;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,14 +16,17 @@ public class StudentController {
     @Autowired
     StudentProxy studentProxy;
 
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
     @GetMapping("/fing_Student")
     public Map<Integer, Student> getStudentMap(){
-
+        logger.info("students -> {}", studentProxy.getStudentMap());
         return studentProxy.getStudentMap();
     }
 
     @GetMapping("/fing_Student/{id}")
     public Student getStudentMap(@PathVariable("id") int id){
+        logger.info("student -> {}",studentProxy.getStuden(id) );
         return studentProxy.getStuden(id);
     }
 
