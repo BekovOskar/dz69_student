70ec38a7bb76a6041d16a6ee24417a93fd9f24a8
BekovOskar <bek.offsll@gmail.com>
2019-06-07 15:08:16 +0600

    added setings for zipkin

--- gateway_student/src/main/java/com/bekov/gateway_student/GatewayStudentApplication.java
+++ gateway_student/src/main/java/com/bekov/gateway_student/GatewayStudentApplication.java
@@ -1,9 +1,11 @@
 package com.bekov.gateway_student;
 
+import brave.sampler.Sampler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableZuulProxy
@@ -14,4 +16,8 @@ public class GatewayStudentApplication {
         SpringApplication.run(GatewayStudentApplication.class, args);
     }
 
+    @Bean
+    public Sampler defaultSampler(){
+        return Sampler.ALWAYS_SAMPLE;
+    }
 }
