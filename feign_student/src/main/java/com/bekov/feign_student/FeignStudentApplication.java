d6b8c5d8e0d5513f2911fb917eaa486726e8972b
BekovOskar <bek.offsll@gmail.com>
2019-06-07 15:07:59 +0600

    added setings for zipkin

--- feign_student/src/main/java/com/bekov/feign_student/FeignStudentApplication.java
+++ feign_student/src/main/java/com/bekov/feign_student/FeignStudentApplication.java
@@ -1,9 +1,11 @@
 package com.bekov.feign_student;
 
+import brave.sampler.Sampler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -14,4 +16,8 @@ public class FeignStudentApplication {
         SpringApplication.run(FeignStudentApplication.class, args);
     }
 
+    @Bean
+    public Sampler defaultSampler(){
+        return Sampler.ALWAYS_SAMPLE;
+    }
 }
